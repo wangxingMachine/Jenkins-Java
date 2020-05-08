62c9f57e07e6c90c1cf3b21261d03904eb15f71c
wangxingmachine <wangxingmachine@gmail.com>
2020-05-08 22:49:34 +0800

    commit

---
+++ src/main/java/com/wangxingmachine/capistrano/CapistranoSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.wangxingmachine.capistrano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapistranoSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapistranoSpringBootApplication.class, args);
+    }
+}
